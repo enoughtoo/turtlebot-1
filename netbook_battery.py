ad6674caab8e2fd10b9ba82e92b923286d9a4a7c
kai <kaizhou.tuwien@gmail.com>
2018-12-06 17:44:44 +0100

    update netbook-charging detection python file to adapt to kinetic

--- netbook_battery.py
+++ netbook_battery.py
@@ -20,7 +20,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 import roslib
 import rospy
-from smart_battery_msgs.msg import SmartBatteryStatus #for netbook battery
+from sensor_msgs.msg import BatteryState #for netbook battery
 
 class netbook_battery():
 
@@ -28,7 +28,7 @@ class netbook_battery():
 		rospy.init_node("netbook_battery")		
 
 		#monitor netbook's battery status.  Everytime anything changes call the call back function self.NetbookPowerEventCallback and pass the data regarding the current battery status
-		rospy.Subscriber("/laptop_charge/",SmartBatteryStatus,self.NetbookPowerEventCallback)
+		rospy.Subscriber("/laptop_charge/",BatteryState,self.NetbookPowerEventCallback)
 
 		#rospy.spin() tells the program to not exit until you press ctrl + c.  If this wasn't there... it'd subscribe to /laptop_charge/ then immediatly exit (therefore stop "listening" to the thread).
 		rospy.spin();
@@ -37,7 +37,7 @@ class netbook_battery():
 	def NetbookPowerEventCallback(self,data):
 		print("Percent: " + str(data.percentage)) 
 		print("Charge: " + str(data.charge))
-		if(int(data.charge_state) == 1):
+		if(int(data.power_supply_status) == 1):
 			print("Currently charging")
 		else:
 			print("Not charging")
